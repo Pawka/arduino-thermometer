dd4e5e711caa24e53b643ac4f4e335b1da34fff3
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2014-02-02 22:59:54 +0200

    Write untill memory full.

--- src/sketch.ino
+++ src/sketch.ino
@@ -4,20 +4,24 @@
 dht11 DHT11;
 
 int EEPROM_ADDR = 0; //Current EEPROM address for write.
-
-int serialData = 0; //Data from serial.
+int EEPROM_END_VALUE = 255; //Describes end of data sequence.
+int EEPROM_SIZE = 10;
+int PIN = 13; //Info pin id.
 
 void setup()
 {
     DHT11.attach(2);
+    pinMode(PIN, OUTPUT);
     Serial.begin(9600);
+    EEPROM_ADDR = getDataSize();
+    Serial.println(EEPROM_ADDR);
 }
 
 void loop()
 {
     int temp = getTemperature();
     storeTemperature(temp);
-    delay(2000);
+    delay(1000);
 }
 
 /**
@@ -32,6 +36,8 @@ void serialEvent()
             commandReset();
         } else if (input == '2') {
             commandReadData();
+        } else if (input == '3') {
+            commandToggleMonitoring();
         }
     }
 }
@@ -55,8 +61,33 @@ int getTemperature()
 
 void storeTemperature(int value)
 {
-    EEPROM.write(EEPROM_ADDR, value);
-    EEPROM_ADDR++;
+    //Write until memory is full. Leave last byte for end value.
+    if (EEPROM_ADDR < EEPROM_SIZE - 1) {
+        digitalWrite(PIN, LOW);
+        EEPROM.write(EEPROM_ADDR, value);
+        EEPROM_ADDR++;
+
+        EEPROM.write(EEPROM_ADDR, EEPROM_END_VALUE);
+    } else {
+        //Turn on pin when memory is full.
+        digitalWrite(PIN, HIGH);
+    }
+}
+
+/**
+ * Look for data stored on EEPROM size. It is possible to write the value on
+ * some EEPROM address but reading is chosen to avoid unnecesasry writes and
+ * extend EEPROM lifetime.
+ */
+int getDataSize()
+{
+    for (int i = 0; i < EEPROM_SIZE; i++) {
+        if (EEPROM.read(i) == EEPROM_END_VALUE) {
+            return i - 1;
+        }
+    }
+
+    return 0;
 }
 
 /**
@@ -65,6 +96,9 @@ void storeTemperature(int value)
 void commandReset()
 {
     EEPROM_ADDR = 0;
+
+    //Write end value to start of memory.
+    EEPROM.write(0, EEPROM_END_VALUE);
 }
 
 /**
@@ -73,9 +107,16 @@ void commandReset()
 void commandReadData()
 {
     Serial.print("Length: ");
-    Serial.println(EEPROM_ADDR + 1);
-    for (int i = 0; i <= EEPROM_ADDR; i++) {
+    Serial.println(EEPROM_ADDR);
+    for (int i = 0; i <= getDataSize(); i++) {
         Serial.println(EEPROM.read(i));
     }
 }
 
+/**
+ * Start/stop temperature monitoring.
+ */
+void commandToggleMonitoring()
+{
+    //@todo implement.
+}
