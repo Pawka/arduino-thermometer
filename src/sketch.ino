4c78ba045c3ee9a5202e7698866c5f609d512802
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2014-02-02 22:15:50 +0200

    Store temperature on EEPROM. Menu for output data to serial.

--- src/sketch.ino
+++ src/sketch.ino
@@ -1,6 +1,11 @@
 #include <dht11.h>
+#include <EEPROM.h>
 
 dht11 DHT11;
+
+int EEPROM_ADDR = 0; //Current EEPROM address for write.
+
+int serialData = 0; //Data from serial.
 
 void setup()
 {
@@ -10,11 +15,32 @@ void setup()
 
 void loop()
 {
-    Serial.println("\n");
+    int temp = getTemperature();
+    storeTemperature(temp);
+    delay(2000);
+}
 
+/**
+ * Event is triggered when data on Serial is available.
+ */
+void serialEvent()
+{
+    if (Serial.available()) {
+        char input = (char)Serial.read();
+
+        if (input == '1') {
+            commandReset();
+        } else if (input == '2') {
+            commandReadData();
+        }
+    }
+}
+
+int getTemperature()
+{
     int chk = DHT11.read();
 
-    Serial.print("Read sensor: ");
+    /*
     switch (chk)
     {
         case 0: Serial.println("OK"); break;
@@ -22,8 +48,34 @@ void loop()
         case -2: Serial.println("Time out error"); break;
         default: Serial.println("Unknown error"); break;
     }
+    */
 
-    Serial.print("Temperature (°C): ");
-    Serial.println((float)DHT11.temperature, DEC);
-    delay(2000);
+    return (int)DHT11.temperature;
 }
+
+void storeTemperature(int value)
+{
+    EEPROM.write(EEPROM_ADDR, value);
+    EEPROM_ADDR++;
+}
+
+/**
+ * Reset program status.
+ */
+void commandReset()
+{
+    EEPROM_ADDR = 0;
+}
+
+/**
+ * Write EEPROM data to serial port.
+ */
+void commandReadData()
+{
+    Serial.print("Length: ");
+    Serial.println(EEPROM_ADDR + 1);
+    for (int i = 0; i <= EEPROM_ADDR; i++) {
+        Serial.println(EEPROM.read(i));
+    }
+}
+
